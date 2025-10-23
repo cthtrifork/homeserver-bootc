4719f82ae873a1f125b709549c965b5067ff5de0
Casper Thygesen <cth@trifork.com>
2025-10-23 20:55:01 +0200

    BatchMode

--- tests/homeserver/user-test.sh
+++ tests/homeserver/user-test.sh
@@ -5,7 +5,7 @@ echo "Running as"
 id
 
 echo "== Github CLI =="
-echo "GitHub token fingerprint: $(printf "%s" "$GITHUB_TOKEN" | cut -c1-7)"
+echo "GitHub token fingerprint: ${GITHUB_TOKEN:0:7}********"
 gh auth status && echo "✅ Github CLI is ready"
 
 echo "== Github SSH Auth =="
@@ -14,7 +14,10 @@ ssh-keygen -y -f ~/.ssh/id_ed25519 | head -c 80; echo
 ssh-keygen -lf ~/.ssh/id_ed25519.pub
 echo "Trying to authenticate..."
 
-output=$(ssh -o StrictHostKeyChecking=accept-new -vT git@github.com 2>&1)
+set +e
+output=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=accept-new -vT git@github.com 2>&1)
+ssh_status=$?
+set -e
 echo "$output"
 
 if echo "$output" | grep -q "successfully"; then
