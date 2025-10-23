62bf5449769fca61df0461c936c48f3f44ea1348
Casper Thygesen <cth@trifork.com>
2025-10-23 20:43:47 +0200

    Ensure local bin is executable

--- system_files/homeserver/usr/libexec/setup-home.sh
+++ system_files/homeserver/usr/libexec/setup-home.sh
@@ -19,21 +19,24 @@ dotfiles_sync(){
     --chmod=F644,D755 \
     --chown="${TARGET_USER}:${TARGET_USER}" \
     /usr/share/dotfiles/ "${HOME_DIR}/"
+
+    # Ensure local bin is executable
+    chmod -R +x "${HOME_DIR}/.local/bin"
 }
 
 # Setup OH-MY-BASH for user
 install_ohmybash() {
   if [[ -f "/usr/local/share/oh-my-bash/bashrc" ]]; then
     # copy local
-    cp /usr/local/share/oh-my-bash/bashrc "$HOME_DIR"/.bashrc
+    cp /usr/local/share/oh-my-bash/bashrc "${HOME_DIR}/.bashrc"
 
     # modify .bashrc
     # https://github.com/ohmybash/oh-my-bash?tab=readme-ov-file
-    sed -i 's/^plugins=(git)$/plugins=(git kubectl)/g' "$HOME_DIR"/.bashrc
-    echo 'export OMB_USE_SUDO=false' >> "$HOME_DIR"/.bashrc
-    echo 'export DISABLE_AUTO_UPDATE=true' >> "$HOME_DIR"/.bashrc
+    sed -i 's/^plugins=(git)$/plugins=(git kubectl)/g' "${HOME_DIR}/.bashrc"
+    echo 'export OMB_USE_SUDO=false' >> "${HOME_DIR}/.bashrc"
+    echo 'export DISABLE_AUTO_UPDATE=true' >> "${HOME_DIR}/.bashrc"
 
-    chown "$TARGET_USER":"$TARGET_USER" "$HOME_DIR"/.bashrc
+    chown "$TARGET_USER":"$TARGET_USER" "${HOME_DIR}/.bashrc"
   fi
 }
 
