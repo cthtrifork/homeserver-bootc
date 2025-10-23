8d1570cc48f41022e8cdda0b477b0f656947c896
Casper Thygesen <cth@trifork.com>
2025-10-23 20:43:41 +0200

    print SHA

--- tests/homeserver/user-test.sh
+++ tests/homeserver/user-test.sh
@@ -9,8 +9,9 @@ echo "GitHub token fingerprint: $(printf "%s" "$GITHUB_TOKEN" | cut -c1-7)"
 gh auth status && echo "✅ Github CLI is ready"
 
 echo "== Github SSH Auth =="
-echo "Public key: "
+echo "Public key and SHA: "
 ssh-keygen -y -f ~/.ssh/id_ed25519 | head -c 80; echo
+ssh-keygen -lf ~/.ssh/id_ed25519.pub
 echo "Trying to authenticate..."
 
 output=$(ssh -o StrictHostKeyChecking=accept-new -vT git@github.com 2>&1)
