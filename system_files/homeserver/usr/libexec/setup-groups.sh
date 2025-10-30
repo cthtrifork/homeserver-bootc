686a7060c95ae968134ede3c8e9154e180883d51
Casper Thygesen <cth@trifork.com>
2025-10-30 08:24:43 +0100

    set default password and disable sshd password authentication (#157)
    * Add check to skip group setup if already completed and default password
    
    * Add SSH configuration to disable password authentication
    
    * USER_NAME fix

--- system_files/homeserver/usr/libexec/setup-groups.sh
+++ system_files/homeserver/usr/libexec/setup-groups.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 mkdir -p /etc/homeserver
 
+if [[ -f /etc/homeserver/setup-groups.done ]]; then
+  echo "Groups already set up, skipping."
+  exit 0
+fi
+
 # Setup Groups
 wheelarray=($(getent group wheel | cut -d: -f4 |  tr ',' '\n'))
 
@@ -15,18 +20,22 @@ mv /etc/subgid /etc/subgid.bak
 
 # Setup home directory for each user in wheel group
 i=0
-for user in "${wheelarray[@]}"; do
-  echo "Setting up home for user: $user"
+for USER_NAME in "${wheelarray[@]}"; do
+  echo "Setting up home for user: $USER_NAME"
 
   start=$((BASE + (i * STEP)))
-  echo "$user:$start:$SIZE" | tee -a /etc/subuid /etc/subgid >/dev/null
+  echo "$USER_NAME:$start:$SIZE" | tee -a /etc/subuid /etc/subgid >/dev/null
 
-  if ! grep -q "^$user:" /etc/passwd; then
-    echo "[WARNING] Fixed user $user in /etc/passwd"
-    getent passwd $user | sudo tee -a /etc/passwd
+  if ! grep -q "^$USER_NAME:" /etc/passwd; then
+    echo "[WARNING] Fixed user $USER_NAME in /etc/passwd"
+    getent passwd $USER_NAME | sudo tee -a /etc/passwd
   fi
 
-  usermod -aG docker "$user"
+  usermod -aG docker "$USER_NAME"
+
+  echo "Password" | passwd $USER_NAME -s
 
   i=$((i + 1))
 done
+
+touch /etc/homeserver/setup-groups.done
