ab6b81f2d12c3830af901c0d13545f389a995904
Casper Thygesen <cth@trifork.com>
2025-12-07 13:18:06 +0100

    again

--- system_files/homeserver/usr/share/bootc/privileged-setup.hooks.d/10-sudoless.sh
+++ system_files/homeserver/usr/share/bootc/privileged-setup.hooks.d/10-sudoless.sh
@@ -4,3 +4,5 @@ echo "Configuring subuid/subgid for $TARGET_USER"
 
 touch /etc/subuid /etc/subgid
 usermod --add-subuid 100000-165535 --add-subgid 100000-165535 $TARGET_USER
+
+podman system migrate
