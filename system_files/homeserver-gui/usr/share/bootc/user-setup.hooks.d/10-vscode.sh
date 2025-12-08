d619cfb80283771a729562b2da1a3de54a1db607
Casper Thygesen <cth@trifork.com>
2025-12-08 09:51:52 +0100

    small fixes

--- system_files/homeserver-gui/usr/share/bootc/user-setup.hooks.d/10-vscode.sh
+++ system_files/homeserver-gui/usr/share/bootc/user-setup.hooks.d/10-vscode.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/bash
-
 set -x
 
-# Setup VSCode
-if test ! -e "$HOME"/.config/Code/User/settings.json; then
-	mkdir -p "$HOME"/.config/Code/User
-	cp -f /etc/skel/.config/Code/User/settings.json "$HOME"/.config/Code/User/settings.json
-fi
+echo "Configuring vscode extensions"
 
 code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
