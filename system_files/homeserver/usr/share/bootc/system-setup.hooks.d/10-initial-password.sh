608def046b3b43dd4528d8fabe2a277875949742
Casper Thygesen <cth@trifork.com>
2025-12-10 13:01:49 +0100

    Update password setup for target user
    Change password setting method for the target user.

--- system_files/homeserver/usr/share/bootc/system-setup.hooks.d/10-initial-password.sh
+++ system_files/homeserver/usr/share/bootc/system-setup.hooks.d/10-initial-password.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 if [ ! -e /etc/passwd.done ]; then
-    echo "Password" | passwd $TARGET_USER -s
+    # Set default password
+    echo "$TARGET_USER:Password" | chpasswd
+    # ensure the account is unlocked
+    usermod -U $TARGET_USER || true
 	#chage -E $(date -d "+1 days" +%Y-%m-%d) $TARGET_USER # force password change on next login
 fi
 
