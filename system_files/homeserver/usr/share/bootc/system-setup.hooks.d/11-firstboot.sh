1b361d5d2499ffad92a0ea2c28e792bdbbb55691
Casper Thygesen <cth@trifork.com>
2025-12-12 15:31:47 +0100

    Update 11-firstboot.sh

--- system_files/homeserver/usr/share/bootc/system-setup.hooks.d/11-firstboot.sh
+++ system_files/homeserver/usr/share/bootc/system-setup.hooks.d/11-firstboot.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 # checks if any regular home areas exist. 
 # If none are found, it searches for service credentials starting with `home.create.` to create users at boot. 
-homectl firstboot
+#homectl firstboot
