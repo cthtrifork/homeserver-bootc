0a80034e9fd8a039e3cd9bec8665644274f2ffb6
Casper Thygesen <cth@trifork.com>
2025-12-13 22:54:47 +0100

    added https proxy test

---
+++ tests/httpsproxy/system-test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Running as"
+id
+
+echo "Verifying status for custom installed services..."
+CORE_SERVICES="squid.service"
+echo "--- core services ---"
+for s in $CORE_SERVICES; do
+    systemctl is-active --quiet "$s" || {
+        echo "Service not active: $s"
+        systemctl status "$s" --no-pager || true
+        exit 1
+    }
+done
+echo "✅ core services are OK"
+
+curl --proxy http://localhost:3128 https://google.com -v
