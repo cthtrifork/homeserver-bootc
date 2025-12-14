eedfff10a88e583cb8576dbcbacd49088746b32c
Casper Thygesen <cth@trifork.com>
2025-12-14 11:55:09 +0100

    Update system-test.sh

--- tests/httpsproxy/system-test.sh
+++ tests/httpsproxy/system-test.sh
@@ -7,6 +7,9 @@ id
 echo "== squid.service status =="
 journalctl -u squid.service -b --no-pager
 systemctl cat squid
+coredumpctl list squid
+coredumpctl info squid
+cat /var/log/squid/* | tail -n 100
 
 echo "Verifying status for custom installed services..."
 CORE_SERVICES="squid.service"
