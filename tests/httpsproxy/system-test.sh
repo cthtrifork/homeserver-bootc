77abf63ecb7cc7e56512185712a17e771191c5f7
Casper Thygesen <cth@trifork.com>
2025-12-16 12:31:52 +0100

    more testing

--- tests/httpsproxy/system-test.sh
+++ tests/httpsproxy/system-test.sh
@@ -20,5 +20,14 @@ echo "✅ core services are OK"
 
 echo "Testing HTTPS proxy through squid..."
 curl --proxy http://localhost:3128 https://google.com -v
+echo "First request done, should be a MISS"
+curl --proxy http://localhost:3128 https://google.com -v
+echo "Second request done, should be a HIT"
 
-curl --proxy https://localhost:3138 https://github.com/getsops/sops/releases/download/v3.11.0/sops-3.11.0-1.x86_64.rpm -v | grep "TCP_HIT/200"
+curl --proxy https://localhost:3138 https://github.com/getsops/sops/releases/download/v3.11.0/sops-3.11.0-1.x86_64.rpm -v
+
+grep "TCP_HIT/200" /var/log/squid/access.log || {
+    echo "HTTPS proxy test failed, no TCP_HIT/200 found in access.log"
+    exit 1
+}
+echo "✅ HTTPS proxy through squid is OK"
