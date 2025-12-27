03bd8ac2a30ecd0fb9a5772877f2df45d1ea9644
Casper Thygesen <cth@trifork.com>
2025-12-27 20:13:50 +0100

    Add cache directory to utilities script

--- build_files/utilities.sh
+++ build_files/utilities.sh
@@ -13,7 +13,7 @@ MACHINE="linux"
 BIN_DIR="/usr/local/bin"
 COMPLETION_DIR="/usr/local/share/bash-completion/completions"
 
-mkdir -p "$BIN_DIR" "$COMPLETION_DIR"
+mkdir -p "$BIN_DIR" "$COMPLETION_DIR" "/tmp/cache"
 
 setfattr -n user.component -v "utilities" /usr/local/bin
 setfattr -n user.component -v "utilities" /usr/local/share/bash-completion
@@ -26,7 +26,7 @@ tmp_name() {
     local ext="$3"
     local safe_version="${version//\//-}"
     safe_version="${safe_version// /_}"
-    echo "/tmp/${name}-${safe_version}.${ext}"
+    echo "/tmp/cache/${name}-${safe_version}.${ext}"
 }
 
 download_if_missing() {
