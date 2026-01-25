43bbc458f7b2e2ff7c32acf2bea50daacfae59c9
Casper Thygesen <cth@trifork.com>
2026-01-25 22:28:41 +0100

    Disable dracut hacks and initramfs update
    Comment out dracut configuration and initramfs update commands.

--- build_files/gui/90-initramfs.sh
+++ build_files/gui/90-initramfs.sh
@@ -4,14 +4,13 @@ set -eou pipefail
 
 # dracut hacks
 # https://gitlab.com/fedora/bootc/tracker/-/issues/66#note_2590604787
-cat /etc/dracut.conf.d/no-xattr.conf || true
-export DRACUT_NO_XATTR=1
+#cat /etc/dracut.conf.d/no-xattr.conf || true
+#export DRACUT_NO_XATTR=1
 
 # Setup Plymouth with theme
 plymouth-set-default-theme spinner
-systemctl enable plymouth-start.service
 
 # Update initramfs
-set -x
-kver=$(cd /usr/lib/modules && echo *)
-dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
+#set -x
+#kver=$(cd /usr/lib/modules && echo *)
+#dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
