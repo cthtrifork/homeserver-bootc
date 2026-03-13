6b2b18df164b911c915bfabdd9f6230ee50fc4a7
Casper Thygesen <cth@trifork.com>
2026-03-13 15:58:09 +0100

    Create 12-yaml-schema-router.sh + fix zed.desktop (#429)

--- system_files/homeserver/usr/share/bootc/user-setup.hooks.d/12-zed.sh
+++ system_files/homeserver/usr/share/bootc/user-setup.hooks.d/12-zed.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Setup ~/.local directories
+mkdir -p "$HOME/.local/bin" "$HOME/.local/share/applications"
+
+appid="dev.zed.Zed"
+
+# Copy .desktop file
+desktop_file_path="$HOME/.local/share/applications/${appid}.desktop"
+src_dir="/usr/share/dotfiles/.local/zed.app/share/applications"
+if [ -f "$src_dir/${appid}.desktop" ]; then
+    cp "$src_dir/${appid}.desktop" "${desktop_file_path}"
+    sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" "${desktop_file_path}"
+    sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/bin/zed|g" "${desktop_file_path}"
+fi
+
 # Link the zed binary
-if [ -f "$HOME/.local/zed$suffix.app/bin/zed" ]; then
-    ln -sf "$HOME/.local/zed$suffix.app/bin/zed" "$HOME/.local/bin/zed"
+if [ -f "$HOME/.local/zed.app/bin/zed" ]; then
+    ln -sf "$HOME/.local/zed.app/bin/zed" "$HOME/.local/bin/zed"
 fi
