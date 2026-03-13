f497822e42c2bf52487b292719f77aec7a25918f
Casper Thygesen <cth@trifork.com>
2026-03-13 15:40:04 +0100

    Update 12-zed.sh

--- system_files/homeserver/usr/share/bootc/user-setup.hooks.d/12-zed.sh
+++ system_files/homeserver/usr/share/bootc/user-setup.hooks.d/12-zed.sh
@@ -11,9 +11,9 @@ desktop_file_path="$HOME/.local/share/applications/${appid}.desktop"
 src_dir="/usr/share/dotfiles/.local/zed.app/share/applications"
 if [ -f "$src_dir/${appid}.desktop" ]; then
     cp "$src_dir/${appid}.desktop" "${desktop_file_path}"
+    sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" "${desktop_file_path}"
+    sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/bin/zed|g" "${desktop_file_path}"
 fi
-sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" "${desktop_file_path}"
-sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/bin/zed|g" "${desktop_file_path}"
 
 # Link the zed binary
 if [ -f "$HOME/.local/zed.app/bin/zed" ]; then
