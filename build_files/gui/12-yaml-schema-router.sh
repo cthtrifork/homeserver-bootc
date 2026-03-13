f82e933c97f861b6468f24d96862cb8f886b86de
Casper Thygesen <cth@trifork.com>
2026-03-13 15:08:13 +0100

    Update 12-yaml-schema-router.sh

--- build_files/gui/12-yaml-schema-router.sh
+++ build_files/gui/12-yaml-schema-router.sh
@@ -17,11 +17,11 @@ PROJECT_NAME="yaml-schema-router"
 platform="$(uname -s)" # Linux
 arch="$(uname -m)"     # x86_64
 
-YAML_SCHEMA_ROUTER_VERSION="0.2.0" # renovate: datasource=github-releases depName=traiproject/yaml-schema-router
+YAML_SCHEMA_ROUTER_VERSION="v0.2.0" # renovate: datasource=github-releases depName=traiproject/yaml-schema-router
 
 # Construct file name based on your .goreleaser.yaml template
 # e.g., yaml-schema-router_v1.0.0_linux_x86_64.tar.gz
-TAR_FILE="${PROJECT_NAME}_${YAML_SCHEMA_ROUTER_VERSION#v}_${OS_NAME}_${ARCH_NAME}.tar.gz"
+TAR_FILE="${PROJECT_NAME}_${YAML_SCHEMA_ROUTER_VERSION}_${OS_NAME}_${ARCH_NAME}.tar.gz"
 DOWNLOAD_URL="https://github.com/${REPO}/releases/download/${YAML_SCHEMA_ROUTER_VERSION}/${TAR_FILE}"
 
 TMP_DIR=$(mktemp -d)
