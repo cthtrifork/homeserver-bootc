66185025177718807b91088d326f4910b49d4693
Casper Thygesen <cth@trifork.com>
2026-09-05 12:07:19 +0200

    Update user-test.sh

--- tests/homeserver/user-test.sh
+++ tests/homeserver/user-test.sh
@@ -15,6 +15,11 @@ echo "== Github CLI =="
 echo "GitHub token fingerprint: ${GITHUB_TOKEN:0:7}********"
 gh auth status && echo "✅ Github CLI is ready"
 
+echo "== Podman =="
+# Rootless
+curl --unix-socket $XDG_RUNTIME_DIR/podman/podman.sock http://localhost/_ping
+# Rootful
+sudo curl --unix-socket /run/podman/podman.sock http://localhost/_ping
 
 echo "== Docker =="
 echo "Checking if user is in docker group"
