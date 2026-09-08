37e53058cb5f437318faf800962e0e34530c0ca1
Casper Thygesen <cth@trifork.com>
2026-09-08 13:50:03 +0200

    Refactor SELinux restorecon command for clarity

--- build_files/gui/22-swtpm-fix.sh
+++ build_files/gui/22-swtpm-fix.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Ensure SELinux permissions
+trap '[[ $BASH_COMMAND != echo* ]] && [[ $BASH_COMMAND != log* ]] && echo "+ $BASH_COMMAND"' DEBUG
+
+echo "::group:: ===$(basename "$0")==="
+
+rm -rf /etc/selinux/targeted/tmp /etc/selinux/targeted/previous
+
 semodule -i /usr/share/selinux/packages/swtpm*.pp
-restorecon -v /usr/bin/swtpm /usr/bin/swtpm_setup
+
+restorecon -v \
+  /usr/bin/swtpm \
+  /usr/bin/swtpm_setup
+
+ls -lZ /usr/bin/swtpm
